bf4c21c470a780d3c146371a979f2440aa3ea94c
Stel Abrego <stel@stel.codes>
2022-02-20 00:02:15 -0800

    Add export

--- src/codes/stel/nuzzle/core.clj
+++ src/codes/stel/nuzzle/core.clj
@@ -14,3 +14,15 @@
   (-> site-config
       (gen/load-site-config)
       (gen/realize-site-config remove-drafts?)))
+
+(defn export
+  "Exports the website to :target-dir. The :static-dir is overlayed on top of
+  the :target-dir after the web pages have been exported."
+  [{:keys [static-dir target-dir] :as global-config}]
+  {:pre [(or (map? global-config) (string? global-config))
+         (string? static-dir)
+         (string? target-dir)]}
+  (log/info "🔨🐈 Exporting static site to disk")
+  (-> global-config
+      (gen/global-config->site-index)
+      (gen/export-site-index static-dir target-dir)))
