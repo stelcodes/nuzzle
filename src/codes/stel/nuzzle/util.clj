38ac5ca7faffada70aa0689ab49d723145a91042
Stel Abrego <stel@stel.codes>
2022-02-20 00:03:14 -0800

    Add util namespace

---
+++ src/codes/stel/nuzzle/util.clj
@@ -0,0 +1,29 @@
+(ns codes.stel.nuzzle.util
+  (:require [clojure.string :as string]))
+
+;; Taken from https://clojuredocs.org/clojure.core/merge
+(defn deep-merge [a & maps]
+  (if (map? a)
+    (apply merge-with deep-merge a maps)
+    (apply merge-with deep-merge maps)))
+
+(defn id->uri
+  [id]
+  {:pre [(vector? id)]}
+  (str "/" (string/join "/" (map name id)) "/"))
+
+(defn kebab-case->title-case
+  [s]
+  (->> (string/split (name s) #"-")
+       (map string/capitalize)
+       (string/join " ")))
+
+(comment
+ (string/split (name :educational-media) #"-")
+ (kebab-case->title-case :educational-media))
+
+(defn kebab-case->lower-case
+  [s]
+  (->> (string/split (name s) #"-")
+       (map string/lower-case)
+       (string/join " ")))
