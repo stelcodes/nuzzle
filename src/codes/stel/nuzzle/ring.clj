96e139915d19c22a91fb8a89f28fb9dafc1843b6
Stel Abrego <stel@stel.codes>
2022-04-18 18:14:10 -0700

    Improve missing static directory error message

--- src/codes/stel/nuzzle/ring.clj
+++ src/codes/stel/nuzzle/ring.clj
@@ -1,5 +1,7 @@
 (ns codes.stel.nuzzle.ring
-  (:require [ring.middleware.file :refer [wrap-file]]))
+  (:require
+   [clojure.java.io :as io]
+   [ring.middleware.file :refer [wrap-file]]))
 
 (defn wrap-static-dir
   [app static-dir]
@@ -7,6 +9,6 @@
     (try
       (wrap-file app static-dir)
       (catch Exception _
-        (throw (ex-info (str "Static directory " static-dir " does not exist")
+        (throw (ex-info (str "Static directory " (.getCanonicalPath (io/file static-dir)) " does not exist")
                         {:static-dir static-dir}))))
     app))
