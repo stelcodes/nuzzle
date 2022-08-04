6edbaa4b3266e3e6111347f60242f596374f0451
Stel Abrego <stel@stel.codes>
2022-08-04 14:48:16 -0700

    Add clojure.spec rough draft to schemas
    Malli is coming up short in regards to being able to fully encapsulate
    the complexity of the Nuzzle config. There is no good way to define a
    map where some values are validated based on the type of the keys.
    clojure.spec is actually much better in this regard so I'll probably be
    switching over soon.

--- src/nuzzle/schemas.clj
+++ src/nuzzle/schemas.clj
@@ -1,4 +1,6 @@
-(ns nuzzle.schemas)
+(ns nuzzle.schemas
+  (:require
+   [clojure.spec.alpha :as s]))
 
 (def local-date
   [:fn {:error/message "should be a date string in format YYYY-MM-DD"
@@ -52,3 +54,22 @@
    [:nuzzle/sitemap? {:optional true} :boolean]
    [:nuzzle/build-drafts? {:optional true} boolean?]
    [:nuzzle/server-port {:optional true} [:and int? [:> 1023] [:< 65536]]]])
+
+;; clojure.spec ideas
+(comment
+ (def datetime? #(try (java.time.LocalDate/parse %) true (catch Throwable _ false)))
+ (s/def :nuzzle/title string?)
+ (s/def :nuzzle/rss? boolean?)
+ (s/def :nuzzle/modified datetime?)
+ (s/def :nuzzle/page-key (s/coll-of keyword? :kind vector?))
+ (s/def :nuzzle/page-val (s/keys :req [:nuzzle/title] :opt [:nuzzle/rss? :nuzzle/modified]))
+ (s/def :nuzzle/config-entry (s/or :page (s/tuple :nuzzle/page-key :nuzzle/page-val)
+                                   :option (s/tuple keyword? any?)))
+ (s/def :nuzzle/user-config
+   (s/and
+    (s/keys :req [:nuzzle/base-url])
+    (s/every :nuzzle/config-entry)))
+ (s/explain
+  :nuzzle/user-config
+  {:nuzzle/base-url "https://test.com"
+   [:blog-posts :test-post] {:nuzzle/title "hi" :nuzzle/rss? true :nuzzle/modified "bad-datetime"}}))
