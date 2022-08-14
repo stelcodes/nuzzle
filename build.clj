fc5385e8f692f6d8f2951ca96506f411fccce809
Stel Abrego <stel@stel.codes>
2022-08-13 17:43:26 -0700

    Add build.clj using clojure.tools.build tooling

---
+++ build.clj
@@ -0,0 +1,55 @@
+(ns build
+  (:require
+   [clojure.string :as str]
+   [clojure.tools.build.api :as b]))
+
+(def lib 'codes.stel/nuzzle)
+(def version (str (-> ".VERSION_PREFIX" slurp str/trim) "." (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn tag [_]
+  (b/git-process {:git-args (list "tag" "-a" "-m" (str "Bump version to v" version) (str "v" version))}))
+
+(defn ensure-tag [_]
+  (when-not (re-find (re-pattern (str "v" version))
+                     (b/git-process {:git-args "tag"}))
+    (throw (ex-info (str "Version " version " not found in git tags") {}))))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (clean nil)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis}))
+
+(defn deploy [opts]
+  (ensure-tag nil)
+  (jar opts)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   (merge {:installer :remote
+           :artifact jar-file
+           :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
+  opts)
