fd6a8489a71e1f67e022f3eaf0533a79e9db7557
Stel Abrego <stel@stel.codes>
2022-08-18 14:09:52 -0700

    Move create-get-config to config ns (#118)
    Move and rename nuzzle.generator/gen-get-config to
    nuzzle.config/create-get-config. This change marks the end of the
    nuzzle.generator namespace and the files are now deleted.

--- src/nuzzle/config.clj
+++ src/nuzzle/config.clj
@@ -7,7 +7,6 @@
    [nuzzle.hiccup :as hiccup]
    [nuzzle.log :as log]
    [nuzzle.schemas :as schemas]
-   [nuzzle.generator :as gen]
    [nuzzle.util :as util]
    ;; Register spell-spec expound helpers after requiring expound.alpha
    [spell-spec.expound]))
@@ -98,6 +97,34 @@
             (assoc acc ckey {:nuzzle/index child-pages :nuzzle/title title})))
         {})))
 
+(defn create-get-config
+  "Create the helper function get-config from the transformed config. This
+  function takes a config key and returns the corresponding value with added
+  key :nuzzle/get-config with value get-config function attached."
+  [config]
+  {:pre [(map? config)] :post [(fn? %)]}
+  (fn get-config
+    ([& ckeys]
+     ;; If no args, return the whole config
+     (if (empty? ckeys)
+       (reduce-kv (fn [acc ckey cval]
+                    (assoc acc ckey
+                           (cond-> cval
+                             ;; Add get-config to all page entry maps
+                             (vector? ckey) (assoc :nuzzle/get-config get-config))))
+                  {} config)
+       (reduce (fn [last-match ckey]
+                 (if (try (contains? last-match ckey) (catch Throwable _ false))
+                   (let [next-match (get last-match ckey)]
+                     (cond-> next-match
+                       ;; Only add get-config to returned value if it's a page entry map
+                       (and (vector? ckey) (map? next-match) (:nuzzle/title next-match))
+                       (assoc :nuzzle/get-config get-config)))
+                   (throw (ex-info (str "get-config error: config key sequence "
+                                        (-> ckeys vec pr-str) " cannot be resolved")
+                                   {:invalid-key ckey}))))
+               config ckeys)))))
+
 (defn transform-config
   "Creates fully transformed config with or without drafts."
   [{:nuzzle/keys [build-drafts?] :as config}]
@@ -140,7 +167,7 @@
                                                            (content/create-render-content-fn ckey config)))))
              {} config))
           (add-get-config-to-pages [config]
-            (let [get-config (gen/gen-get-config config)]
+            (let [get-config (create-get-config config)]
               (reduce-kv
                (fn [acc ckey cval]
                  (assoc acc ckey (cond-> cval
