cd10f261574cf8f295bd30f674e870173503b71b
Stel Abrego <stel@stel.codes>
2022-08-18 12:28:13 -0700

    Move create-*-index-page-entries funcs to config ns (#118)
    Move create-tag-index-page-entries and
    create-hierarchical-index-page-entries to nuzzle.config as part of the
    process of removing the nuzzle.generator namespace.

--- test/nuzzle/config_test.clj
+++ test/nuzzle/config_test.clj
@@ -5,6 +5,8 @@
 
 (def config-path "test-resources/edn/config-1.edn")
 
+(defn config [] (conf/read-config-from-path config-path))
+
 (def config-2-path "test-resources/edn/config-2-bad.edn")
 
 (def render-page (constantly [:h1 "test"]))
@@ -66,3 +68,42 @@
           error-str (with-out-str (try (conf/validate-config config) (catch Throwable _ nil)))]
       (is (re-find #"Spec failed" error-str))
       (is (re-find #"->\W*config\W*:nuzzle\/author-registry\W*keys\W*set" error-str)))))
+
+(deftest create-tag-index-page-entries
+  (is (= {[:tags :bar]
+          {:nuzzle/index #{[:blog :foo]},
+           :nuzzle/title "#bar"}
+          [:tags :baz]
+          {:nuzzle/index #{[:blog :foo]},
+           :nuzzle/title "#baz"}}
+         (conf/create-tag-index-page-entries {[:blog :foo] {:nuzzle/tags #{:bar :baz}} [:about] {}})))
+  (is (= {[:tags :nuzzle]
+          {:nuzzle/index #{[:blog :nuzzle-rocks] [:blog :why-nuzzle]},
+           :nuzzle/title "#nuzzle"}
+          [:tags :colors]
+          {:nuzzle/index #{[:blog :favorite-color]},
+           :nuzzle/title "#colors"}}
+         (conf/create-tag-index-page-entries (config)))))
+
+(deftest create-hierarchical-index-page-entries
+  (is (= {[:blog-posts]
+          {:nuzzle/index #{[:blog-posts :foo] [:blog-posts :archive]}, :nuzzle/title "Blog Posts"},
+          [:blog-posts :archive]
+          {:nuzzle/index #{[:blog-posts :archive :baz]},
+           :nuzzle/title "Archive"}
+          [:projects]
+          {:nuzzle/index #{[:projects :bee]}, :nuzzle/title "Projects"}
+          []
+          {:nuzzle/index #{[:blog-posts] [:projects]}
+           :nuzzle/title "Home"}}
+         (conf/create-hierarchical-index-page-entries {[:blog-posts :foo] {:nuzzle/title "Foo"}
+                                  [:blog-posts :archive :baz] {:nuzzle/title "Baz"}
+                                  [:projects :bee] {:nuzzle/title "Bee"}})))
+  (is (= {[:blog]
+          {:nuzzle/index
+           #{[:blog :why-nuzzle] [:blog :favorite-color] [:blog :nuzzle-rocks]},
+           :nuzzle/title "Blog"}
+          []
+          {:nuzzle/index #{[:about] [:blog]}
+           :nuzzle/title "Home"}}
+         (conf/create-hierarchical-index-page-entries (config)))))
