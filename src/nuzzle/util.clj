f9235406823dcd61421d05648163cbd1eb515c75
Stel Abrego <stel@stel.codes>
2022-09-10 11:54:21 -0700

    Move clj-commons/digest to test dependencies (#106)
    Move code using the library into the test/ source tree so it won't get
    included in library JAR.

--- src/nuzzle/util.clj
+++ src/nuzzle/util.clj
@@ -1,8 +1,6 @@
 (ns nuzzle.util
   (:require
    [babashka.fs :as fs]
-   [clj-commons.digest :as digest]
-   [clojure.data :as data]
    [clojure.java.shell :as sh]
    [clojure.pprint :as pprint]
    [clojure.string :as string]))
@@ -91,36 +89,6 @@
        (or (and (string? item) (re-find regex item) (reduced item))
            (and desc-result (reduced desc-result)))))
    hiccup))
-
-(defn dir-contents-map
-  "Create a datastructure representing a directory's structure and contents in
-  order to compare it with another directory. Creates a map of: relative paths
-  (string) -> md5 checksums (string) of all the files inside the directory.
-  Nested directory paths are not checksummed and have a value of :dir"
-  [dir]
-  {:pre [(-> dir (java.io.File.) (.isDirectory))]}
-  (let [files (-> dir (java.io.File.) file-seq)
-        parent-path-name-count (-> files first (.toPath) (.getNameCount))]
-    (reduce (fn [contents-map file]
-              (let [abs-path (.toPath file)
-                    path-name-count (.getNameCount abs-path)
-                    rel-path (str (.subpath abs-path parent-path-name-count path-name-count))
-                    md5-checksum (if (.isDirectory (.toFile abs-path))
-                                   :dir
-                                   (digest/md5 file))]
-                (assoc contents-map rel-path md5-checksum)))
-            (sorted-map) (rest files))))
-
-(defn diff-dirs
-  "Determine if two directories have the same file structure and content. If
-  differences, return list of relative filenames that are different and log
-  diffs. If identical, return nil"
-  [dir1 dir2]
-  (let [cm1 (dir-contents-map dir1)
-        cm2 (dir-contents-map dir2)
-        [d1 d2 _] (data/diff cm1 cm2)
-        mismatches (-> (merge d1 d2) keys)]
-    mismatches))
 
 (defn path->last-mod-inst
   "Returns Instant of last modified time of a file specified by path"
--- test/nuzzle/publish_test.clj
+++ test/nuzzle/publish_test.clj
@@ -1,6 +1,8 @@
 (ns nuzzle.publish-test
   (:require
    [babashka.fs :as fs]
+   [clj-commons.digest :as digest]
+   [clojure.data :as data]
    [clojure.string :as str]
    [clojure.test :refer [deftest is]]
    [nuzzle.config :as conf]
@@ -11,6 +13,36 @@
 (def config-path "test-resources/edn/config-1.edn")
 
 (defn config [] (conf/load-config-from-path config-path))
+
+(defn dir-contents-map
+  "Create a datastructure representing a directory's structure and contents in
+  order to compare it with another directory. Creates a map of: relative paths
+  (string) -> md5 checksums (string) of all the files inside the directory.
+  Nested directory paths are not checksummed and have a value of :dir"
+  [dir]
+  {:pre [(-> dir (java.io.File.) (.isDirectory))]}
+  (let [files (-> dir (java.io.File.) file-seq)
+        parent-path-name-count (-> files first (.toPath) (.getNameCount))]
+    (reduce (fn [contents-map file]
+              (let [abs-path (.toPath file)
+                    path-name-count (.getNameCount abs-path)
+                    rel-path (str (.subpath abs-path parent-path-name-count path-name-count))
+                    md5-checksum (if (.isDirectory (.toFile abs-path))
+                                   :dir
+                                   (digest/md5 file))]
+                (assoc contents-map rel-path md5-checksum)))
+            (sorted-map) (rest files))))
+
+(defn diff-dirs
+  "Determine if two directories have the same file structure and content. If
+  differences, return list of relative filenames that are different and log
+  diffs. If identical, return nil"
+  [dir1 dir2]
+  (let [cm1 (dir-contents-map dir1)
+        cm2 (dir-contents-map dir2)
+        [d1 d2 _] (data/diff cm1 cm2)
+        mismatches (-> (merge d1 d2) keys)]
+    mismatches))
 
 (deftest create-sitemap
   (is (= (-> "test-resources/xml/empty-sitemap.xml" slurp str/trim)
@@ -64,7 +96,7 @@
                    (assoc :nuzzle/publish-dir (str temp-site-dir))
                    (update :nuzzle/overlay-dir #(str "test-resources/" %)))
         _ (publish/publish-site config {:deterministic? true})
-        mismatches (util/diff-dirs temp-site-dir reference-site-dir)]
+        mismatches (diff-dirs temp-site-dir reference-site-dir)]
     (doseq [mismatch mismatches
             :let [rel->abs-path (fn [parent-dir path] (str parent-dir "/" path))
                   diff-cmd ["diff" (rel->abs-path temp-site-dir mismatch)
