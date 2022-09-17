3d580192a8a4a6bf268730272cf5c01ef5173286
Stel Abrego <stel@stel.codes>
2022-09-17 06:15:44 -0700

    Replace old dir diffing with new functions
    Now the nuzzle.publish-test ns uses the new directory diffing functions.

--- test/nuzzle/publish_test.clj
+++ test/nuzzle/publish_test.clj
@@ -1,45 +1,12 @@
 (ns nuzzle.publish-test
   (:require
    [babashka.fs :as fs]
-   [clj-commons.digest :as digest]
-   [clojure.data :as data]
    [clojure.string :as str]
    [clojure.test :refer [deftest is]]
    [nuzzle.pages :as pages]
-   [nuzzle.log :as log]
    [nuzzle.publish :as publish]
    [nuzzle.test-util :as test-util]
    [nuzzle.util :as util]))
-
-(defn dir-contents-map
-  "Create a datastructure representing a directory's structure and contents in
-  order to compare it with another directory. Creates a map of: relative paths
-  (string) -> md5 checksums (string) of all the files inside the directory.
-  Nested directory paths are not checksummed and have a value of :dir"
-  [dir]
-  {:pre [(-> dir (java.io.File.) (.isDirectory))]}
-  (let [files (-> dir (java.io.File.) file-seq)
-        parent-path-name-count (-> files first (.toPath) (.getNameCount))]
-    (reduce (fn [contents-map file]
-              (let [abs-path (.toPath file)
-                    path-name-count (.getNameCount abs-path)
-                    rel-path (str (.subpath abs-path parent-path-name-count path-name-count))
-                    md5-checksum (if (.isDirectory (.toFile abs-path))
-                                   :dir
-                                   (digest/md5 file))]
-                (assoc contents-map rel-path md5-checksum)))
-            (sorted-map) (rest files))))
-
-(defn diff-dirs
-  "Determine if two directories have the same file structure and content. If
-  differences, return list of relative filenames that are different and log
-  diffs. If identical, return nil"
-  [dir1 dir2]
-  (let [cm1 (dir-contents-map dir1)
-        cm2 (dir-contents-map dir2)
-        [d1 d2 _] (data/diff cm1 cm2)
-        mismatches (-> (merge d1 d2) keys)]
-    mismatches))
 
 (deftest create-sitemap
   (is (= (-> "test-resources/xml/empty-sitemap.xml" slurp str/trim)
@@ -47,7 +14,8 @@
 
 (deftest publish-site
   (let [temp-site-dir (str (fs/create-temp-dir))
-        reference-site-dir (str (fs/path "test-resources/sites/twin-peaks"))
+        reference-dir (str (fs/path "test-resources/sites/twin-peaks"))
+        reference-snapshot (util/create-dir-snapshot reference-dir)
         pages (-> test-util/twin-peaks-pages pages/load-pages)
         atom-feed {:title "Foo's blog"
                    :author (test-util/authors :donna)
@@ -57,14 +25,8 @@
                             :base-url "https://foobar.com"
                             :publish-dir temp-site-dir
                             :atom-feed atom-feed))
-    (let [mismatches (diff-dirs temp-site-dir reference-site-dir)]
-      (doseq [mismatch mismatches
-              :let [rel->abs-path (fn [parent-dir path] (str parent-dir "/" path))
-                    diff-cmd ["diff" (rel->abs-path temp-site-dir mismatch)
-                              (rel->abs-path reference-site-dir mismatch)]
-                    {:keys [out err]} (apply util/safe-sh diff-cmd)]]
-        (log/warn "Found mismatch:" mismatch)
-        (apply println "+" diff-cmd)
-        (println out)
-        (println err))
-      (is (nil? mismatches)))))
+    (let [site-snapshot (util/create-dir-snapshot temp-site-dir)
+          diff (util/create-dir-diff reference-snapshot site-snapshot)]
+      (when-not (every? empty? (vals diff))
+        (-> (util/safe-sh "diff" "-r" reference-dir temp-site-dir) :out println))
+      (is (every? empty? (vals diff))))))
