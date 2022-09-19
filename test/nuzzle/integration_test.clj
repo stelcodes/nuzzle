5bda0e6d6dc5ecf36f6548a16855b958987a5f91
Stel Abrego <stel@stel.codes>
2022-09-19 08:45:35 -0700

    Reintroduce integration tests for example directory

--- test/nuzzle/integration_test.clj
+++ test/nuzzle/integration_test.clj
@@ -1,84 +1,21 @@
-;; (ns nuzzle.integration-test
-;;   (:require
-;;    [clojure.string :as str]
-;;    [clojure.test :refer [deftest is]]
-;;    [cybermonday.core :as cm]
-;;    [nuzzle.config :as conf]
-;;    [nuzzle.hiccup :as hiccup]
-;;    [nuzzle.log :as log]
-;;    [nuzzle.util :as util]))
+(ns nuzzle.integration-test
+  (:require
+   [babashka.fs :as fs]
+   [clojure.java.io :as io]
+   [clojure.java.shell :as sh]
+   [clojure.test :as t]
+   [nuzzle.util :as util]))
 
-;; (defn render-page
-;;   [{:nuzzle/keys [render-content]}]
-;;   [:html [:body (render-content)]])
-;;
-;; (declare ^:dynamic md->hiccup)
-;;
-;; (defn read-ash-config []
-;;   (try
-;;     (let [ash-regex #"How I Caught Pikachu"
-;;           ash-code (-> "README.md"
-;;                        slurp
-;;                        cm/parse-body
-;;                        (util/find-hiccup-str ash-regex)
-;;                        (str/replace "markdown/" "test-resources/markdown/")
-;;                        (str/replace "md->hiccup" "nuzzle.hiccup/md->hiccup"))
-;;           config-start (.indexOf ash-code "(defn config")
-;;           config-str (subs ash-code config-start)
-;;           config-fn (binding [md->hiccup hiccup/md->hiccup]
-;;                       (-> config-str read-string eval))]
-;;       (-> (config-fn)
-;;           (assoc :nuzzle/render-page render-page)))
-;;     (catch Throwable e
-;;       (log/error "Could not read example config function in README.md")
-;;       (throw e))))
-;;
-;; (comment (read-ash-config))
-;;
-;; (defn normalize-loaded-config
-;;   "Calls the :nuzzle/render-content function in each page entry value so it's
-;;   easier to test equality"
-;;   [config]
-;;   {:pre [(map? config)]}
-;;   (let [trigger-render-content
-;;         (fn [cval]
-;;           (if-not (:nuzzle/render-content cval)
-;;             cval
-;;             (update cval :nuzzle/render-content #(%))))
-;;         remove-get-config
-;;         (fn [cval]
-;;           (cond-> cval
-;;             (map? cval) (dissoc :nuzzle/get-config)))]
-;;     (-> config
-;;         (update-vals trigger-render-content)
-;;         (update-vals remove-get-config))))
-;;
-;; (comment (-> (read-ash-config) normalize-loaded-config))
-;;
-;; (deftest transform-config
-;;   (let [config (-> (read-ash-config) conf/load-pages)
-;;         normalized-config (normalize-loaded-config config)]
-;;     ;; Check that every page entry has a :nuzzle/get-config key
-;;     (is (every? (fn [[ckey cval]] (or (not (vector? ckey)) (contains? cval :nuzzle/get-config)))
-;;                 config))
-;;     (is (= normalized-config
-;;            {:nuzzle/render-page render-page
-;;             []
-;;             {:nuzzle/title "Home"
-;;              :nuzzle/index #{[:about] [:blog-posts]},
-;;              :nuzzle/url []
-;;              :nuzzle/render-content '([:h1 {:id "placeholder"} "Placeholder"])},
-;;             [:blog-posts :catching-pikachu]
-;;             {:nuzzle/title "How I Caught Pikachu",
-;;              :nuzzle/url [:blog-posts :catching-pikachu]
-;;              :nuzzle/render-content '([:h1 {:id "placeholder"} "Placeholder"])},
-;;             [:about]
-;;             {:nuzzle/title "About Ash"
-;;              :nuzzle/url [:about]
-;;              :nuzzle/render-content '([:h1 {:id "placeholder"} "Placeholder"])},
-;;             [:blog-posts]
-;;             {:nuzzle/index
-;;              #{[:blog-posts :catching-pikachu]},
-;;              :nuzzle/title "Blog Posts",
-;;              :nuzzle/url [:blog-posts]
-;;              :nuzzle/render-content '([:p {} "Hi I'm Ash and this is my blog!"])}}))))
+(t/deftest example-sites
+  (doseq [example-dir (-> "examples" io/file (.listFiles))]
+    (let [prev-build-path (-> example-dir (fs/path "dist") fs/canonicalize str)
+          prev-build-snapshot (util/create-dir-snapshot prev-build-path)
+          new-build-path (str (fs/create-temp-dir))
+          {:keys [exit out]} (sh/sh "bash" "-c"
+                                    (str "cd " example-dir " && clj -T:site publish :publish-dir '\"" new-build-path "\"'"))
+          new-build-snapshot (util/create-dir-snapshot new-build-path)
+          diff (util/create-dir-diff prev-build-snapshot new-build-snapshot)]
+      (t/is (zero? exit))
+      (t/is (re-find #"Publishing successful" out))
+      (t/is (every? #(-> % val empty?) diff))
+      (println "diff -r" prev-build-path new-build-path))))
