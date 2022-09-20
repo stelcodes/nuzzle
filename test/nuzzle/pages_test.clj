ecf40372bd782b255d2b9a92875b6c0effcf26de
Stel Abrego <stel@stel.codes>
2022-09-19 17:45:04 -0700

    Remove old commented test

--- test/nuzzle/pages_test.clj
+++ test/nuzzle/pages_test.clj
@@ -4,21 +4,6 @@
    [nuzzle.pages :as pages]
    [nuzzle.test-util :as test-util]))
 
-;; (deftest validate-config
-;;   (testing "bad config fails with expound output"
-;;     (let [error-str (with-out-str (try (-> test-util/config-2 conf/load-config)
-;;                                     (catch Throwable _ nil)))]
-;;       (is (re-find #"Spec failed" error-str))
-;;       (is (re-find #"should contain key:.{6}:nuzzle/base-url" error-str))))
-;;   (testing "author registry author validation works"
-;;     (let [config {:nuzzle/render-page nuzzle.test-util/render-page
-;;                   :nuzzle/author-registry {:laura {:name "Laura Palmer"}}
-;;                   [:blog :douglas-firs] {:nuzzle/title "Douglas Firs Smell Really Freakin Good"
-;;                                          :nuzzle/author :agent-cooper}}
-;;           error-str (with-out-str (try (conf/validate-config config) (catch Throwable _ nil)))]
-;;       (is (re-find #"Spec failed" error-str))
-;;       (is (re-find #"->\W*config\W*:nuzzle\/author-registry\W*keys\W*set" error-str)))))
-
 (deftest remove-draft-pages
   (let [x {[:foo :bar]
            {:nuzzle/draft? true}}]
