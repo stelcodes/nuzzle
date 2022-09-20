17d9fd61bbe362978281bf663186dba245836bb6
Stel Abrego <stel@stel.codes>
2022-09-19 17:44:35 -0700

    Update ash ketchum blog dist directory

--- examples/ash-ketchum-blog/site.clj
+++ examples/ash-ketchum-blog/site.clj
@@ -17,7 +17,7 @@
             [:li [:a {:href url} title]])])
    (render-content)]])
 
-;; Here we make a function to create a render-content function for markdown files
+;; Here we make a function that creates a render-content function that turns markdown files into hiccup
 (defn md-content [md-path]
   #(-> md-path slurp nuzz/parse-md))
 
@@ -25,47 +25,55 @@
 (def ash {:name "Ash Ketchum"
           :email "ashketchum@fastmail.com"})
 
-(defn pages []
-  (-> {[]
-       {:nuzzle/title "Home"
-        :nuzzle/render-content (md-content "content/intro.md")
-        :nuzzle/render-page render-page
-        :nuzzle/index :children}
+(def pages
+  {[]
+   {:nuzzle/title "Home"
+    :nuzzle/render-content (md-content "content/intro.md")
+    :nuzzle/render-page render-page
+    :nuzzle/index :children}
 
-       [:blog-posts]
-       {:nuzzle/title "Blog Posts"
-        :nuzzle/render-page render-page
-        :nuzzle/index :children}
+   [:about]
+   {:nuzzle/title "About Me"
+    :nuzzle/render-content (md-content "content/about.md")
+    :nuzzle/render-page render-page}
 
-       [:blog-posts :catching-pikachu]
-       {:nuzzle/title "I Caught a Pikachu!"
-        :nuzzle/render-content (md-content "content/caught-pikachu.md")
-        :nuzzle/render-page render-page
-        :nuzzle/author ash
-        :nuzzle/feed? true}
+   [:blog-posts]
+   {:nuzzle/title "Blog Posts"
+    :nuzzle/render-page render-page
+    :nuzzle/index :children}
 
-       [:blog-posts :defeating-misty]
-       {:nuzzle/draft? true
-        :nuzzle/title "Misty's Pokemon Got Wrecked by Pikachu"
-        :nuzzle/render-content (md-content "content/defeated-misty.md")
-        :nuzzle/render-page render-page
-        :nuzzle/author ash
-        :nuzzle/feed? true}
+   [:blog-posts :catching-pikachu]
+   {:nuzzle/title "I Caught a Pikachu!"
+    :nuzzle/render-content (md-content "content/caught-pikachu.md")
+    :nuzzle/render-page render-page
+    :nuzzle/tags #{:pokedex}
+    :nuzzle/author ash
+    :nuzzle/feed? true}
 
-       [:about]
-       {:nuzzle/title "About Me"
-        :nuzzle/render-content (md-content "content/about.md")
-        :nuzzle/render-page render-page}}
-      (nuzz/add-tag-pages render-page)))
+   [:blog-posts :defeating-misty]
+   {:nuzzle/draft? true
+    :nuzzle/title "Misty's Pokemon Got Wrecked by Pikachu"
+    :nuzzle/render-content (md-content "content/defeated-misty.md")
+    :nuzzle/render-page render-page
+    :nuzzle/tags #{:gym-battle}
+    :nuzzle/author ash
+    :nuzzle/feed? true}
+
+   [:tags]
+   {:nuzzle/title "Tags"
+    :nuzzle/render-page render-page
+    :nuzzle/index :children}})
 
 (defn develop [_]
-  (nuzz/develop #'pages :port 8080))
+  (nuzz/develop #'pages {:remove-drafts? false
+                         :tag-pages {:render-page render-page}}))
 
 ;; By default build this site with a sitemap and Atom feed
 ;; Overlay the directory containing the css/main.css file.
 
 (defn publish [_]
   (nuzz/publish pages {:base-url "https://ashketchum.com"
+                       :tag-pages {:render-page render-page}
                        :atom-feed {:title "Ash Ketchum's Blog"
                                    :subtitle "In a world we must defend"}
                        :overlay-dir "public"}))
